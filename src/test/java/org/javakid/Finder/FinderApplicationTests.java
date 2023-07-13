cb1907ab5f746c42f4de76d267375eb834e26f2a
kutyavin <dmitr.kutyavin@x5.ru>
2023-07-13 16:29:58 +0700

    Initial commit

---
+++ src/test/java/org/javakid/Finder/FinderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javakid.Finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
